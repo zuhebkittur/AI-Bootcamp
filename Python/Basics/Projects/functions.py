79666ac0bae45a41316d9b8ae39fa3ca66898609
Zuheb Kittur <zuhebkittur14@gmail.com>
2026-07-07 00:33:47 +0530

    Refactored Weather App using functions

---
+++ Python/Basics/Projects/functions.py
@@ -0,0 +1,20 @@
+# def greet():
+#     print("Hello Zuheb")
+# def bye():
+#     print("Goodbye Zuheb!")
+
+# greet()
+# bye()
+
+# def greet(name):
+#     print(f"Hello {name}")
+
+# greet("zuheb")
+# greet("ALi")
+# greet("sara")
+
+# def get_weather(city):
+#     print(f"Getting weather for {city}...")
+
+# city = input("Enter the city: ")
+# get_weather(city)
